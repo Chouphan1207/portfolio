3c805a38ea1199de9da8ca22d678453900853055
Phan Hoang Trong Tin <Chouphan1207@gmail.com>
2025-06-02 08:31:23 +0700

    Update Contact Form

--- components/ContactForm.tsx
+++ components/ContactForm.tsx
@@ -147,7 +147,7 @@ export function ContactForm() {
             <BottomGradient />
           </button>
           <button
-            className="group/btn shadow-input relative flex h-10 w-full items-center justify-start space-x-2 rounded-md bg-gray-50 px-4 font-medium text-black dark:bg-zinc-900 dark:shadow-[0px_0px_1px_1px_#262626] transition duration-300 hover:brightness-150"
+            className="group/btn shadow-input relative flex h-10 w-full items-center justify-start space-x-2 rounded-md bg-gradient-to-br from-black to-neutral-900 px-4 font-medium text-black  dark:shadow-[0px_0px_1px_1px_#262626] transition duration-300 hover:brightness-150"
             type="button"
           >
             <IconBrandGoogle className="h-4 w-4 text-neutral-800 dark:text-neutral-300" />
